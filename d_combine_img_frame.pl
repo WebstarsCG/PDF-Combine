c6d087ceb9f7d56b128e65b9f6b6d02a15c9ea57
Raja Renga Bashyam <raja@webstarscg.com>
2024-08-23 05:48:23 +0530

    [UPD] update in format removal of default intention

--- d_combine_img_frame.pl
+++ d_combine_img_frame.pl
@@ -1,56 +1,56 @@
 #!/usr/bin/perl -w 
         
-        # set local lib path where PDF combine resides          
-        BEGIN{ use lib 'lib/'; }
-        
-        use CGI::Carp qw/fatalsToBrowser/;
-        
-        # include lib
-        use PDF::Combine;
-        
-        print "Content-type:text/html\n\n";
-        
-        # PDF::combine object instance                            
-        my $d = new PDF::Combine();
-        
-        # set content dir
-        $d->setContentDir("content/jpg/web");
-        
-        # set frame        
-        $d->setFrame("./content/frame/frame_1399w_746h.pdf");
-        
-        # set global padding to the image
-        $d->setImagePadding({'top'=>100});
-        
-        # set output file
-        $d->setFileOut("pdf_out/combine_img_frame.pdf");          
-        
-        # add global content to show page title from the file name
-        $d->addContent({'color'     => '#121212',         # text color  
-                        'font'      => 'Arial-bold',      # font
-                        'font_size' => 14,                # text size       
-                        'text'      => {'key'=>'title'},  # default key for get page name information 
-                        'x'         => 35,
-                        'y'         => 55});                    
-                              
-        # content to show current page no
-        $d->addContent({'color'     => '#454545',
-                        'font_size' => 11,                                               
-                        'text'      => {'key'=>'page_no'},
-                        'x'         => 1145,
-                        'y'         => 55});
-                         
-                        
-        # custom text
-        $d->addContent({
-                        'color'     => '#454545',
-                        'font_size' => 11,                        
-                        'font'      =>'Helvetica-bold',  
-                        'text'      => ' | Fibenis-CMS',
-                        'x'         => 1160,
-                        'y'         => 55});
-                
+# set local lib path where PDF combine resides          
+BEGIN{ use lib 'lib/'; }
 
-        # do the combine process
-        print (($d->process())?"File created with combining of images in a pdf frame with page title &raquo; "."<a href=".$d->getFileOut()." target='_blank'>Open File</a><br>":"Error");
+use CGI::Carp qw/fatalsToBrowser/;
+
+# include lib
+use PDF::Combine;
+
+print "Content-type:text/html\n\n";
+
+# PDF::combine object instance                            
+my $d = new PDF::Combine({'fileNameFormat' =>'IN'});
+
+# set content dir
+$d->setContentDir("content/jpg/zoho/admin_console_user");
+
+# set frame        
+$d->setFrame("content/frame/frame_1399w_868h.pdf");
+
+# set global padding to the image
+$d->setImagePadding({'top'=>100});
+
+# set output file
+$d->setFileOut("pdf_out/combine_img_frame.pdf");          
+
+# add global content to show page title from the file name
+$d->addContent({'color'     => '#121212',         # text color  
+				'font'      => 'Arial-bold',      # font
+				'font_size' => 18,                # text size       
+				'text'      => {'key'=>'title'},  # default key for get page name information 
+				'x'         => 35,
+				'y'         => 55});                    
+					  
+# content to show current page no
+$d->addContent({'color'     => '#454545',
+				'font_size' => 11,                                               
+				'text'      => {'key'=>'page_no'},
+				'x'         => 1145,
+				'y'         => 55});
+				 
+				
+# custom text
+$d->addContent({
+				'color'     => '#454545',
+				'font_size' => 11,                        
+				'font'      =>'Helvetica-bold',  
+				'text'      => ' |  Zoho Mail',
+				'x'         => 1160,
+				'y'         => 55});
+		
+
+# do the combine process
+print (($d->process())?"File created with combining of images in a pdf frame with page title &raquo; "."<a href=".$d->getFileOut()." target='_blank'>Open File</a><br>":"Error");
               
--- e_combine_img_ordered.pl
+++ e_combine_img_ordered.pl
@@ -1,31 +1,31 @@
 #!/usr/bin/perl -w 
         
-        # set local lib path where PDF combine resides           
-        BEGIN{ use lib './lib/'; }
-        
-        # include lib
-        use PDF::Combine;
-        
-        use CGI::Carp qw/fatalsToBrowser/;       
-                    
-        # PDF::combine object instance with configuration        
-        my $d = new PDF::Combine({  # set path of directory where jpg resides
-                                    'contentDir'   => "content/jpg/web_ordered",
-                                    
-                                    'fileNameFormat' =>'IN',
-                                    
-                                    # set path for new pdf file creation
-                                    'fileOut'      => "pdf_out/combine_img_ordered.pdf" });                                
-        
-        ###################################################################################################################################
-          
-        print "Content-type:text/html\n\n";
-        
-        # do the combine process
-        print (($d->process())?"File created with combining pdf's &raquo; "."<a href=".$d->getFileOut()." target='_blank'>Open File</a><br>":"Error");
-        
-        
-        __END__
+# set local lib path where PDF combine resides           
+BEGIN{ use lib './lib/'; }
+
+# include lib
+use PDF::Combine;
+
+use CGI::Carp qw/fatalsToBrowser/;       
+			
+# PDF::combine object instance with configuration        
+my $d = new PDF::Combine({  # set path of directory where jpg resides
+							'contentDir'   => "content/jpg/web_ordered",
+							
+							'fileNameFormat' =>'IN',
+							
+							# set path for new pdf file creation
+							'fileOut'      => "pdf_out/combine_img_ordered.pdf" });                                
+
+###################################################################################################################################
+  
+print "Content-type:text/html\n\n";
+
+# do the combine process
+print (($d->process())?"File created with combining pdf's &raquo; "."<a href=".$d->getFileOut()." target='_blank'>Open File</a><br>":"Error");
+
+
+__END__
         
         
         
